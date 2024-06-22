60b3695cad9c45c3c5db1962d49c0c401a3122f4
1sisodiyaji <637golusingh@gmail.com>
2024-06-22 18:06:20 +0530

    commit

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express();
 const userRoutes = require("./routes/user");
 // Middleware to parse JSON
 app.use(express.json());
+const cors = require('cors');
 
 Database();
 
 app.use("/api/", userRoutes);
-
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
